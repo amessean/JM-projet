7713345ff047da7e6bf0ea6e6c868da250844b1e
Arthur Messean <arthur.messean@gmail.com>
2020-09-09 17:10:13 +0200

    about fixed

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,5 @@
 class StaticController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :show ]
   skip_after_action :verify_authorized
 
   def show
